16d5a0a1a0b28d03749eff97e19db491ddc21534
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2023-07-06 10:52:03 +0700

    sua lan 3

--- src/main/java/com/example/demo20/config/UrlAuthenticationSuccessHandler.java
+++ src/main/java/com/example/demo20/config/UrlAuthenticationSuccessHandler.java
@@ -23,7 +23,7 @@ public class UrlAuthenticationSuccessHandler implements AuthenticationSuccessHan
 
 
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         handle(request, response, authentication);
         clearAuthenticationAttributes(request);
     }
