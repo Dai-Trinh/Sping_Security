992cced0ab537389ed5d6bad5c47975ec3c3bab3
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2023-07-06 10:03:37 +0700

    sua lan 2

--- src/main/java/com/example/demo20/controller/TestController1.java
+++ src/main/java/com/example/demo20/controller/TestController1.java
@@ -1,4 +1,17 @@
 package com.example.demo20.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController1 {
+
+
+    @GetMapping("/test1git")
+    public String test1git(){
+        return "Test 1 Git";
+    }
+
 }
